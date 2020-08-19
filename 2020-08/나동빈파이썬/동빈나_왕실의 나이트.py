2da1d3fdfa96cbff1d6e8b278593051dd0e978bc
Gihyeon-Kwon <rlg1133@naver.com>
2020-08-19 21:49:15 +0900

    chap 4

--- 2020-08/나동빈파이썬/동빈나_왕실의 나이트.py
+++ 2020-08/나동빈파이썬/동빈나_왕실의 나이트.py
@@ -1,24 +1,49 @@
 n = input()
 
-print(type(n[0]))
-print(type(n[1]))
-if n[0] == 'a':
-    n[0] = 1
-elif n[0] == 'b':
-    n[0] = 2
-elif n[0] == 'c':
-    n[0] = 3
-elif n[0] == 'd':
-    n[0] = 4
-elif n[0] == 'e':
-    n[0] = 5
-elif n[0] == 'f':
-    n[0] = 6
-elif n[0] == 'g':
-    n[0] = 7
-elif n[0] == 'h':
-    n[0] = 8
-
-
+m = []
+n = list(n)
 count = 0
 
+if n[0] == 'a':
+    m.append(1)
+    m.append(int(n[1]))
+elif n[0] == 'b':
+    m.append(2)
+    m.append(int(n[1]))
+elif n[0] == 'c':
+    m.append(3)
+    m.append(int(n[1]))
+elif n[0] == 'd':
+    m.append(4)
+    m.append(int(n[1]))
+elif n[0] == 'e':
+    m.append(5)
+    m.append(int(n[1]))
+elif n[0] == 'f':
+    m.append(6)
+    m.append(int(n[1]))
+elif n[0] == 'g':
+    m.append(7)
+    m.append(int(n[1]))
+elif n[0] == 'h':
+    m.append(8)
+    m.append(int(n[1]))
+
+if m[0] -2 >= 1 and m[1] -1 >= 1:
+    count = count+1
+if m[0] -2 >= 1 and m[1] +1 <= 8:
+    count = count + 1
+if m[0] +2 <= 8 and m[1] +1 <= 8:
+    count = count + 1
+if m[1] +2 <= 8 and m[0] -1 >= 1:
+    count = count + 1
+if m[0] -1 >= 1 and m[1] -2 >= 1:
+    count = count+1
+if m[0] -1 >= 1 and m[1] +2 <= 8:
+    count = count + 1
+if m[0] +1 <= 8 and m[1] +2 <= 8:
+    count = count + 1
+if m[1] +1 <= 8 and m[0] -2 >= 1:
+    count = count + 1
+
+print(count)
